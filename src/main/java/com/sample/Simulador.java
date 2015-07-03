de0782d7025319c16e99dd4fb4a58ef832740763
Tiago Andre Ferreira Pereira <highbugs@gmail.com>
2015-07-03 19:14:13 +0100

    class para pedidos de informacao... Mais tarde passa a gráfica

---
+++ src/main/java/com/sample/Simulador.java
@@ -0,0 +1,15 @@
+package com.sample;
+
+import com.sample.model.*;
+
+public class Simulador {
+	 public static final void main(String[] args) {
+		System.out.println("Welcome to Simulator|Irá ser em parte gráfica|");
+		
+	 }
+	 public Cliente getInformacaoCliente(){
+		 Cliente x = new Cliente(null, 0, false, 0, 0, 0, null);
+		 System.out.pintln("Nome:");
+		 return x;
+	 }
+}
