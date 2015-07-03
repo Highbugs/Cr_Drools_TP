cb1babb358e939f1ede05d23a2f6960876bb4451
Tiago Andre Ferreira Pereira <highbugs@gmail.com>
2015-07-03 17:39:18 +0100

    Estrutura Cliente estudante e fiador adicionados ao modelo

---
+++ src/main/java/com/sample/model/Estudante.java
@@ -0,0 +1,53 @@
+package com.sample.model;
+
+public class Estudante extends Pessoa {
+	private int anoEscolaridade;
+	private float mediaCurso;
+	private boolean creditosIguais;
+	private boolean matriculadoUniversidade;
+	private Cliente fiador; 
+
+	public Estudante(String nome, int idade, boolean historicoIncumprimento,
+			int anoEscolaridade, float mediaCurso, boolean creditosIguais,
+			boolean matriculadoUniversidade) {
+		super(nome, idade, historicoIncumprimento);
+		this.anoEscolaridade = anoEscolaridade;
+		this.mediaCurso = mediaCurso;
+		this.creditosIguais = creditosIguais;
+		this.matriculadoUniversidade = matriculadoUniversidade;
+	}
+	
+	public Cliente getFiador() {
+		return fiador;
+	}
+
+	public void setFiador(Cliente fiador) {
+		this.fiador = fiador;
+	}
+
+	public int getAnoEscolaridade() {
+		return anoEscolaridade;
+	}
+	public void setAnoEscolaridade(int anoEscolaridade) {
+		this.anoEscolaridade = anoEscolaridade;
+	}
+	public float getMediaCurso() {
+		return mediaCurso;
+	}
+	public void setMediaCurso(float mediaCurso) {
+		this.mediaCurso = mediaCurso;
+	}
+	public boolean isCreditosIguais() {
+		return creditosIguais;
+	}
+	public void setCreditosIguais(boolean creditosIguais) {
+		this.creditosIguais = creditosIguais;
+	}
+	public boolean isMatriculadoUniversidade() {
+		return matriculadoUniversidade;
+	}
+	public void setMatriculadoUniversidade(boolean matriculadoUniversidade) {
+		this.matriculadoUniversidade = matriculadoUniversidade;
+	}
+
+}
---
+++ src/main/java/com/sample/model/Pessoa.java
@@ -0,0 +1,31 @@
+package com.sample.model;
+
+public class Pessoa {
+	private String nome;
+	private int idade;
+	private boolean historicoIncumprimento;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Pessoa(String nome, int idade, boolean historicoIncumprimento) {
+		super();
+		this.nome = nome;
+		this.idade = idade;
+		this.historicoIncumprimento = historicoIncumprimento;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public boolean isHistoricoIncumprimento() {
+		return historicoIncumprimento;
+	}
+	public void setHistoricoIncumprimento(boolean historicoIncumprimento) {
+		this.historicoIncumprimento = historicoIncumprimento;
+	}
+}
